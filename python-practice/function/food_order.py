74cb0c30cba64c9cd4924f9d737d9f966aa62202
Imran Ansari <care.imran1@gmail.com>
2026-05-13 17:59:46 +0530

    Food Order Function

--- python-practice/function/food_order.py
+++ python-practice/function/food_order.py
@@ -1,5 +1,8 @@
-def food_order(item, quantity, delivery=True) :
-    print(delivery)
+def food_order(price, item, quantity, delivery=True) :
+    if(delivery) :
+        print(f" {item} X {quantity} - Ghar pe aayega - Total price : {quantity * price}")
+    else: 
+        print(f" {item} X {quantity} - Khud lene aao - Total price : {quantity * price}")
     
-food_order("Biryani", 2)
-food_order("Chai", 3, False)
+food_order(200, "Biryani", 2)
+food_order(20, "Chai", 3, False)
